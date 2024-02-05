bd6931fd1166784a05eaddc019eadfae48463f9d
Guilherme Araújo <132411540+Slotov-7@users.noreply.github.com>
2024-02-04 21:55:27 -0300

    add

---
+++ estudosJavaUdemy/src/aplicacoes/ProblemaCPoo.java
@@ -0,0 +1,40 @@
+package aplicacoes;
+
+import entites.Triangulo;
+
+import java.util.Scanner;
+
+import entites.Triangulo;
+
+public class ProblemaCPoo {
+    public static void main(String[] args) {
+        double areaX, areaY;
+
+        Scanner sc = new Scanner(System.in);
+        Triangulo x,y;
+        x = new Triangulo();
+        y = new Triangulo();
+
+        System.out.println("Digite as medidas do triângulo X: ");
+        x.ladoA = sc.nextDouble();
+        x.ladoB = sc.nextDouble();
+        x.ladoC = sc.nextDouble();
+
+        System.out.println("Digite as medidas do triângulo Y: ");
+        y.ladoA = sc.nextDouble();
+        y.ladoB = sc.nextDouble();
+        y.ladoC = sc.nextDouble();
+
+        areaX = x.area();
+        areaY = y.area();
+
+        System.out.printf("A área do triangulo x é %.2f %n", areaX);
+        System.out.printf("A área do triangulo Y é %.2f %n", areaY);
+        if(areaX > areaX) {
+            System.out.println("A maoir area é do X");
+        }else{
+            System.out.println("A maior area é do Y");
+        }
+        sc.close();
+    }
+}
---
+++ estudosJavaUdemy/src/entites/Triangulo.java
@@ -0,0 +1,11 @@
+package entites;
+public class Triangulo {
+    public double ladoA;
+    public double ladoB;
+    public double ladoC;
+
+    public double area(){
+        double p = (ladoA + ladoB + ladoC) / 2.0;
+        return  Math.sqrt(p * (p - ladoA) * (p - ladoB) * (p - ladoC));
+    }
+}
